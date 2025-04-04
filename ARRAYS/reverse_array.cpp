6df548ca9d667b197c99e2cf1aa2fb3ae2569249
Alyster Benedict <156236282+AlysterBenedict@users.noreply.github.com>
2025-04-04 23:54:05 +0530

    modified:   ARRAYS/reverse_array.cpp 	modified:   ARRAYS/reverse_array.exe 	renamed:    ARRAYS/samllest_largest.cpp -> ARRAYS/smallest_largest.cpp

--- ARRAYS/reverse_array.cpp
+++ ARRAYS/reverse_array.cpp
@@ -1,37 +1,39 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int size;
 
-
-void reverse(int arr[])
+// Function to reverse the array
+void reverse(int arr[], int size)
 {
-    int start=0,end=size-1;
-    while(start<end)
+    int start = 0, end = size - 1;
+    while (start < end)
     {
-        swap(arr[start],arr[end]);
+        swap(arr[start], arr[end]);
         start++;
         end--;
     }
 }
 
-
 int main()
 {
-    
-    
-    cout<<"Enter the size of the array : ";
-    cin>>size;
+    int size;
+    cout << "Enter the size of the array: ";
+    cin >> size;
+
     int arr[size];
-    for(int i=0;i<size;i++)
+    for (int i = 0; i < size; i++)
     {
-        cout<<"Enter the element at the pos "<<(i+1)<<" : ";
-        cin>>arr[i];
+        cout << "Enter the element at the position " << (i + 1) << ": ";
+        cin >> arr[i];
     }
 
-    reverse(arr);
+    // Call the reverse function and pass size as a parameter
+    reverse(arr, size);
 
-    for(int i=0;i<size;i++)
+    cout << "Reversed array: ";
+    for (int i = 0; i < size; i++)
     {
-        cout<<arr[i]<<" ";
+        cout << arr[i] << " ";
     }
+
+    return 0;
 }
