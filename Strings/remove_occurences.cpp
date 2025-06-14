f0e858b5f8d7602b973779260767d1435d26f80a
Alyster Benedict <156236282+AlysterBenedict@users.noreply.github.com>
2025-06-14 17:27:49 +0530

    modified:   Strings/remove_occurences.cpp 	modified:   Strings/remove_occurences.exe

--- Strings/remove_occurences.cpp
+++ Strings/remove_occurences.cpp
@@ -2,18 +2,37 @@
 #include <string>
 using namespace std;
 
-void remove(string &s, const string &pattern) {
-    size_t pos = s.find(pattern); 
-    while (pos != string::npos) {
-        s.erase(pos, pattern.length()); 
-        pos = s.find(pattern, pos); 
+class Solution {
+public:
+    string removeOccurrences(string &s, const string &pattern) {
+        size_t pos;
+        while ((pos = s.find(pattern)) != string::npos) {
+            s.erase(pos, pattern.length());
+            // Backtrack to catch newly formed patterns
+            pos = pos >= pattern.length() ? pos - pattern.length() : 0;
+        }
+        return s;
     }
-}
+};
 
 int main() {
-    string s = "abbbaaacccbbbacbabcabcaabbccabc";
-    string pattern ="abc";
-    remove(s, pattern);
-    cout << s << endl;
+    Solution sol;
+    string input_str, pattern;
+
+    while (true) {
+        cout << "Enter the string (or 'q' to quit): ";
+        getline(cin, input_str);
+        
+        if (input_str == "q" || input_str == "Q") {
+            break;
+        }
+
+        cout << "Enter the pattern to remove: ";
+        getline(cin, pattern);
+
+        string result = sol.removeOccurrences(input_str, pattern);
+        cout << "Result: " << result << endl << endl;
+    }
+
     return 0;
 }
