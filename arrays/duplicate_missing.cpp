b8170c5417f3fe26d09a6a15a8c126494f6e4c93
Alyster Benedict <156236282+AlysterBenedict@users.noreply.github.com>
2025-10-11 23:54:02 +0530

    modified:   arrays/duplicate_missing.cpp

--- arrays/duplicate_missing.cpp
+++ arrays/duplicate_missing.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution  {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
         int n = grid.size();
