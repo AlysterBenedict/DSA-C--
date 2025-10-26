c74f90d7b7f6363926af22c52af8758b50397978
Alyster Benedict <156236282+AlysterBenedict@users.noreply.github.com>
2025-10-26 17:11:37 +0530

    container with most water

--- arrays/container_most_water.cpp
+++ arrays/container_most_water.cpp
@@ -3,56 +3,27 @@ using namespace std;
 
 class Solution {
 public:
-    /**
-     * @brief Finds the container with the most water using the Two Pointer technique.
-     * @param height A vector of integers representing the heights of vertical lines.
-     * @return The maximum area of water that can be contained.
-     */
     int maxArea(vector<int>& height) {
-        // Get the number of lines
-        int n = height.size();
+        int n= height.size();
+        int left =0;
+        int right = n-1;
+        int maxArea=0;
         
-        // Initialize pointers
-        int left = 0;      // Pointer at the start of the array
-        int right = n - 1; // Pointer at the end of the array
         
-        // Variable to store the maximum area found so far
-        int maxArea = 0;
-        
-        // Loop until the pointers cross
-        while (left < right) {
-            // Calculate the width of the current container
-            int width = right - left;
-            
-            // Calculate the height of the water, limited by the shorter line
-            int h = min(height[left], height[right]);
-            
-            // Calculate the area of the current container
-            int currentArea = width * h;
-            
-            // Update the maximum area if the current one is larger
-            maxArea = max(maxArea, currentArea);
-            
-            // --- The Core Logic ---
-            // We need to move the pointer that points to the shorter line.
-            // Why? Because moving the taller line's pointer will definitely
-            // result in a smaller or equal area (width decreases, and
-            // height is still limited by the same shorter line).
-            // Moving the shorter line's pointer gives us a *chance* to find
-            // a taller line, which *might* compensate for the decrease in width.
-            if (height[left] < height[right]) {
-                left++;
-            } else {
-                // This handles both cases:
-                // 1. height[right] < height[left]
-                // 2. height[right] == height[left]
-                // In case 2, it doesn't matter which one we move.
-                right--;
-            }
-        }
-        
-        // Return the final maximum area found
-        return maxArea;
+
+        while(left < right)
+        {
+            int width=right-left;
+            int h= min(height[left], height[right]);
+            int currentArea= width *h;
+            maxArea= max(maxArea, currentArea);
+
+            if(height[left] < height[right])
+            left++;
+            else
+            right--;
+
+        }return maxArea;
     }
 };
 
